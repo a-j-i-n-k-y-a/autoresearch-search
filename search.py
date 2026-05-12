526ba4e7dc6294f1382957c4cd770fd111e5b5ca
a-j-i-n-k-y-a <ajinkyamawal123@gmail.com>
2026-05-12 11:58:50 +0530

    experiment: # Simplify to raw FAISS search

--- search.py
+++ search.py
@@ -2,5 +2,5 @@ import numpy as np
 
 def search(query, df, bm25, model, index, top_k=10):
     query_vec = model.encode([query])
-    distances, top_idx = index.search(query_vec.astype("float32"), top_k)
-    return df.iloc[top_idx[0]][["title", "overview"]].to_dict("records")
+    _, top_idx = index.search(query_vec.astype("float32"), top_k)
+    return df.iloc[top_idx[0]].to_dict("records")
