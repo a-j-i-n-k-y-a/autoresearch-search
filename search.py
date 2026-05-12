ca13b370fec426a208cfd889208475c84ad67cdc
a-j-i-n-k-y-a <ajinkyamawal123@gmail.com>
2026-05-12 09:52:49 +0530

    experiment: # Vector search only

--- search.py
+++ search.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 def search(query, df, bm25, model, index, top_k=10):
-    tokenized = query.lower().split()
-    scores = bm25.get_scores(tokenized)
-    top_idx = np.argsort(scores)[-top_k:][::-1]
-    return df.iloc[top_idx][["title", "overview"]].to_dict("records")
+    vec = model.encode([query])
+    _, top_idx = index.search(vec.astype("float32"), top_k)
+    return df.iloc[top_idx[0]][["title", "overview"]].to_dict("records")
