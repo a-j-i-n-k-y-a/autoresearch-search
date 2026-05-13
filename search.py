75fcfc7247d82d7e5d1926a6e443fd23a5e3f78e
a-j-i-n-k-y-a <ajinkyamawal123@gmail.com>
2026-05-14 01:50:41 +0530

    experiment: # Pure FAISS minimal search

--- search.py
+++ search.py
@@ -3,4 +3,4 @@ import numpy as np
 def search(query, df, bm25, model, index, top_k=10):
     query_vec = model.encode([query])
     _, top_idx = index.search(query_vec.astype("float32"), top_k)
-    return df.iloc[top_idx[0]][["title", "overview"]].to_dict("records")
+    return df.iloc[top_idx[0]].to_dict("records")
