a2896b1eab80bc3a4520db117aef34c81d6b243a
a-j-i-n-k-y-a <ajinkyamawal123@gmail.com>
2026-05-14 20:05:26 +0530

    experiment: # Minimal FAISS search

--- search.py
+++ search.py
@@ -1,18 +1,6 @@
 import numpy as np
 
 def search(query, df, bm25, model, index, top_k=10):
-    bm25_scores = bm25.get_scores(query.lower().split())
-    top_n = 50
-    candidates_idx = np.argsort(bm25_scores)[-top_n:][::-1]
-    
-    candidates = df.iloc[candidates_idx].copy()
-    
-    query_vec = model.encode([query])
-    candidate_vecs = model.encode(candidates['text'].tolist())
-    
-    similarities = np.dot(candidate_vecs, query_vec.T).flatten()
-    
-    candidates['score'] = similarities
-    ranked = candidates.sort_values(by="score", ascending=False)
-    
-    return ranked.head(top_k)[["title", "overview"]].to_dict("records")
+    query_vec = model.encode([query]).astype("float32")
+    _, idxs = index.search(query_vec, top_k)
+    return df.iloc[idxs[0]].to_dict("records")
